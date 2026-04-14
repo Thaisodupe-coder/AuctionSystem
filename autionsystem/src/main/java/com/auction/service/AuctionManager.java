9e3aacfc046c243d7681d760e357e86e7ef421ee
vuuwu <25020431@vnu.edu.vn>
2026-04-14 09:01:03 +0700

    fix unreachable code

--- autionsystem/src/main/java/com/auction/service/AuctionManager.java
+++ autionsystem/src/main/java/com/auction/service/AuctionManager.java
@@ -1,9 +1,10 @@
 package com.auction.service;
+import java.util.ArrayList;
 import java.util.List;
 import com.auction.model.auction.Auction;
 public class AuctionManager {
     private static volatile AuctionManager INSTANCE;
-    private List<Auction> auctions;
+    private List<Auction> auctions = new ArrayList<>();
     private AuctionManager() {}
     public static AuctionManager getINSTANCE() {
         if (INSTANCE == null) {
@@ -17,8 +18,8 @@ public class AuctionManager {
     }
     public void addAuction(Auction auction) {
         if (auction == null) {
-            throw new IllegalArgumentException("Auction cannot be null");
-        auctions.add(auction);
+            return;
         }
+        auctions.add(auction);
     }
 }
